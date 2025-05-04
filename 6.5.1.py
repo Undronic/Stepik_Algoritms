7dad87058b9e31a0b62147f324f0cc4e228227b6
Undronic <andrei.kvcc@gmail.com>
2025-05-05 00:46:03 +0300

    6.5.1

--- 6.5.1.py
+++ 6.5.1.py
@@ -1,4 +1,6 @@
 import bisect
+import random
+
 
 def Partition(A, l, r):
     j = l
@@ -19,19 +21,47 @@ def QuickSort(A, l, r):
     QuickSort(A, x + 1, r)
 
 
+def Partition3(A, l, r):
+    pivot_idx = random.randint(l, r)
+    A[l], A[pivot_idx] = A[pivot_idx], A[l]
+    j = l
+    k = l
+    x = A[l]
+    for i in range(l + 1, r + 1):
+        if A[i] < x:
+            j += 1
+            k += 1
+            A[i], A[k] = A[k], A[i]
+            A[k], A[j] = A[j], A[k]
+        elif A[i] == x:
+            k += 1
+            A[i], A[k] = A[k], A[i]
+    A[l], A[j] = A[j], A[l]
+    return j, k
+
+
+def QuickSort3(A, l, r):
+
+    while l < r:
+        x, z = Partition3(A, l, r)
+        if x - l < r - z:
+            QuickSort3(A, l, x - 1)
+            l = z + 1
+        else:
+            QuickSort3(A, z + 1, r)
+            r = x - 1
+
+
 m, n = map(int, input().split())
 
 segmentsStart, segmentsEnd = map(list, zip(*(map(int, input().split()) for _ in range(m))))
 
-#dots = list(map(int, input().split()))
-
-QuickSort(segmentsStart, 0, m - 1)
+QuickSort3(segmentsStart, 0, m - 1)
 #segmentsStart.sort()
-QuickSort(segmentsEnd, 0, m - 1)
+QuickSort3(segmentsEnd, 0, m - 1)
 #segmentsEnd.sort()
 
 for _ in map(int, input().split()):
     i = bisect.bisect_right(segmentsStart, _)
     j = bisect.bisect_left(segmentsEnd, _)
     print(i - j, end=' ')
-
